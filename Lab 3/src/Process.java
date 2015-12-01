36716a568c0156e62e9c668ee7c3fe96d7220b58
Richard Carragher <carraghr@gmail.com>
2015-12-01 22:16:00 +0000

    Three search terms and scanner complete.

--- Lab 3/src/Process.java
+++ Lab 3/src/Process.java
@@ -11,114 +11,13 @@ import java.util.Set;
 public class Process {
 	
 	public static void main(String [] args){
-		
-		StringBuilder contentBuilder = new StringBuilder();
-		try {
-		    BufferedReader in = new BufferedReader(new FileReader("SearchServlet.html"));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		    	str = str.replace("\n", "").replace("\r", ""); //remove new lines
-		    	
-		    	if(str.contains("<div class=\"ResultURLStyle\">")){
-		    		str = str.substring(str.indexOf("<br>") + 4);
-		    		while(!str.contains("</div><div class=\"ResultSnippetStyle\">") && str != null){
-		    			str = str.replace("\n", "").replace("\r", "");
-		    			
-		    			contentBuilder.append(str);
-				       // System.out.print(str);
-		    			str = in.readLine();    			
-		    		}
-		    		
-		    		str = str.substring(0, str.indexOf("</div><div class=\"ResultSnippetStyle\">"));
-		    		str = str.replace("\n", "").replace("\r", "");
-		    		str = str.replace("  ", ",");
-		    		str+=" ";
-		    		contentBuilder.append(str);
-		    	}
-		    }
-		    in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String content = contentBuilder.toString();
-		
-		String [] allTerm = content.split(" ");
-		
-		HashMap<String,Term> terms = new HashMap<>();
-		for(int i = 0, j = 1; j < allTerm.length; i=j+1, j=i+1){
-			String term = allTerm[i].substring(0, allTerm[i].indexOf(":"));
-			double idf = Double.parseDouble(allTerm[j]);
-			if(terms.containsKey(term)){
-				Term  temp= terms.get(term);
-				temp.addFreq();
-				terms.put(term, temp);
-			}else{/* Add a new */
-				Term temp = new Term(term);
-				temp.getNi(idf);
-				terms.put(term,temp);
-			}
-		}
-		
-		String [] cycleTerms = terms.keySet().toArray(new String[terms.size()]);
-		
-		//fine the top ten results.
-		
-		//get all results for everything
-		for(String term:cycleTerms){
-			Term temp = terms.get(term);
-			temp.setWeight(10);
-			temp.getRank();
-			terms.put(term,temp);
-		}
-		
-		//fine the top ten results.
-		double [] values = new double[10];
-		String [] topTerms = new String[10]; 
-		
-		//just to be safe pop top ten with first 10 values.
-		
-		for(int i = 0; i<10;i++){
-			values[i]= terms.get(cycleTerms[i]).getRank();
-			topTerms[i]=terms.get(cycleTerms[i]).getName();
-		}
-		
-		
-		for(String term:cycleTerms){
-			Term temp = terms.get(term);
-			double value = temp.getRank();
-			
-			for(int i =0; i<values.length;i++){
 				
-				/* We check to see if the value is greater then the value at the top of the list 
-				 * and move through each making sure nothing is smaller then this value rank.*/
-				if(value > values[i]){//move everything that is in this slot down one.
-					
-					double tempValue = values[i];
-					String tempTerm = topTerms[i];
-					
-					for(int j=i+1; j<values.length;j++){
-						
-						double temp2 = values[j];
-						String temp3 = topTerms[j];
-						
-						values[j] = tempValue;
-						topTerms[j] = tempTerm;
-						
-						tempValue = temp2;
-						tempTerm = temp3;
-						
-					}
-					
-					values[i]=value;
-					topTerms[i]=temp.getName();
-					break;
-				}
-			}
-		}
+		getResultPage("cosmic%20event","10");//405
+		System.out.println();
+		getResultPage("Parkinson's%20disease","10");//406
+		System.out.println();
+		getResultPage("tropical%20storms","10");//408
 		
-		for(int i=0; i<values.length;i++){
-			System.out.println(topTerms[i] + ": " + values[i] );
-		}
 	}
 	
 	static void getResultPage(String query, String numberOfDocs){
@@ -131,20 +30,111 @@ public class Process {
 
 			URLConnection connection = url.openConnection();
 	       	BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-			String inputLine;
-
-			while ((inputLine = in.readLine()) != null) 
-				System.out.println(inputLine);
-
-		in.close();
+	       	StringBuilder contentBuilder = new StringBuilder();
+			String str;
+			
+			while ((str = in.readLine()) != null) {
+				str = str.replace("\n", "").replace("\r", ""); //remove new lines
+	    	
+		    	if(str.contains("<div class=\"ResultURLStyle\">")){
+		    		str = str.substring(str.indexOf("<br>") + 4);
+		    		while(!str.contains("</div><div class=\"ResultSnippetStyle\">") && str != null){
+		    			str = str.replace("\n", "").replace("\r", "");
+		    			
+		    			contentBuilder.append(str);
+		    			// System.out.print(str);
+		    			str = in.readLine();    			
+		    		}
+		    		
+		    		str = str.substring(0, str.indexOf("</div><div class=\"ResultSnippetStyle\">"));
+		    		str = str.replace("\n", "").replace("\r", "");
+		    		str = str.replace("  ", ",");
+		    		str+=" ";
+		    		contentBuilder.append(str);
+		    	}
+			}
+		    in.close();
+		    
+		    String content = contentBuilder.toString();
 		
-		}catch(MalformedURLException malformedurlexception){
-		    malformedurlexception.printStackTrace();
-		}catch(IOException ioexception){
-		    ioexception.printStackTrace();
-		}catch(Exception ioexception){
-		    ioexception.printStackTrace();
-		}
-    }
+		    String [] allTerm = content.split(" ");
+		
+		    HashMap<String,Term> terms = new HashMap<>();
+		    for(int i = 0, j = 1; j < allTerm.length; i=j+1, j=i+1){
+		    	String term = allTerm[i].substring(0, allTerm[i].indexOf(":"));
+		    	double idf = Double.parseDouble(allTerm[j]);
+		    	if(terms.containsKey(term)){
+		    		Term  temp= terms.get(term);
+		    		temp.addFreq();
+		    		terms.put(term, temp);
+		    	}else{/* Add a new */
+		    		Term temp = new Term(term);
+		    		temp.getNi(idf);
+		    		terms.put(term,temp);
+		    	}
+		    }
+		
+		    String [] cycleTerms = terms.keySet().toArray(new String[terms.size()]);
+		
+		    //fine the top ten results.
+		
+		    //get all results for everything
+		    for(String term:cycleTerms){
+		    	Term temp = terms.get(term);
+		    	temp.setWeight(10);
+		    	temp.getRank();
+		    	terms.put(term,temp);
+		    }
+		
+		    //fine the top ten results.
+		    double [] values = new double[10];
+		    String [] topTerms = new String[10]; 
+		
+		    //just to be safe pop top ten with first 10 values.
+		
+		    for(int i = 0; i<10;i++){
+		    	values[i]= terms.get(cycleTerms[i]).getRank();
+		    	topTerms[i]=terms.get(cycleTerms[i]).getName();
+		    }
+		
+		    for(String term:cycleTerms){
+		    	Term temp = terms.get(term);
+		    	double value = temp.getRank();
+			
+		    	for(int i =0; i<values.length;i++){
+				
+		    		/* We check to see if the value is greater then the value at the top of the list 
+		    		 * and move through each making sure nothing is smaller then this value rank.*/
+		    		if(value > values[i]){//move everything that is in this slot down one.
+					
+		    			double tempValue = values[i];
+		    			String tempTerm = topTerms[i];
+					
+		    			for(int j=i+1; j<values.length;j++){
+						
+		    				double temp2 = values[j];
+		    				String temp3 = topTerms[j];
+						
+		    				values[j] = tempValue;
+		    				topTerms[j] = tempTerm;
+						
+							tempValue = temp2;
+							tempTerm = temp3;
+						
+						}
+					
+						values[i]=value;
+						topTerms[i]=temp.getName();
+						break;
+					}
+				}
+			}
+		
+			for(int i=0; i<values.length;i++){
+				System.out.println(topTerms[i] + ": " + values[i] );
+			}
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+	}
 }
